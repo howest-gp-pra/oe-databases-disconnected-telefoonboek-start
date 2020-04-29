74c02854dc87cccb1f83414c8e6c79b181df3d74
maximlesy <maximlesy@gmail.com>
2020-04-29 11:27:45 +0200

    Initial commit

---
+++ Oe-pra-db1-telefoonboek.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows;
+using System.Collections;
+using System.Data;
+using System.IO;
+using System.Windows.Controls;
+
+namespace Oe_pra_db1_telefoonboek.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
