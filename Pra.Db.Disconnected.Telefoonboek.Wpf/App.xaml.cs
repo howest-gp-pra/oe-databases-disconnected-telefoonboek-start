6052402d4a3bb965410b579c651105564f5a2d33
Herman De Beukelaer <herman.de.beukelaer@howest.be>
2020-11-05 17:16:39 +0100

    Convert to .NET Core

--- Pra.Db.Disconnected.Telefoonboek.Wpf/App.xaml.cs
+++ Pra.Db.Disconnected.Telefoonboek.Wpf/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace Oe_pra_db1_telefoonboek.Wpf
+namespace Pra.Db.Disconnected.Telefoonboek.Wpf
 {
     /// <summary>
     /// Interaction logic for App.xaml
